29d259da37f0d3aafc1ba0c1a2333cb905e2b3c4
zapashcanon <leo@ndrs.fr>
2022-01-11 04:42:07 +0100

    switch to dune

--- default.nix
+++ default.nix
@@ -3,14 +3,26 @@
 
 let
   myemacs =
-    with pkgs.emacsPackages; pkgs.emacsWithPackages
-      [ haskellMode magit emacsw3m tuaregMode ];
+    with pkgs.emacsPackages; with pkgs.emacsPackagesNg; pkgs.emacsWithPackages
+    [ ace-jump-mode company helm-projectile magit
+      (merlin.overrideAttrs (attrs: {
+         meta = attrs.meta // { broken = false; };
+       }))
+      merlin-company paredit tuareg undo-tree
+    ];
 in with pkgs; stdenv.mkDerivation {
   name = "monadlib";
-  buildInputs = [
-    ocamlPackages.findlib ocamlPackages.ocaml_batteries ocamlPackages.merlin
-    ocamlPackages.ocaml_oasis ocamlPackages.ocaml
-    opam myemacs ];
-  findlib = "${ocamlPackages.findlib}/lib/ocaml/4.01.0/site-lib/";
-  merlin  = "${ocamlPackages.merlin}";
+  buildInputs = [ dune_2
+                  ocaml
+                  ocamlPackages.batteries
+                  ocamlPackages.findlib
+                  ocamlPackages.merlin
+                  ocamlPackages.ocaml_oasis
+                  ocamlPackages.ocaml
+
+                  myemacs
+                ];
+  merlinbin = ocamlPackages.merlin;
+  ocamlformat = pkgs.ocamlformat;
+  utop = ocamlPackages.utop;
 }
